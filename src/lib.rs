b234ff4dcee5b514aeb78d5ffc5336f372a39d0c
evergreen-trading-systems <4870868+evergreen-trading-systems@users.noreply.github.com>
2023-01-11 00:24:44 +0000

    refactor: use lib_auth_id

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use macros_make_error::make_error2;
+use std::str::FromStr;
+
+make_error2!(AuthIdError);
+
+#[derive(
+    utoipa::ToSchema, Debug, PartialEq, serde::Deserialize, serde::Serialize, Clone, Default,
+)]
+pub struct WalletAuthId {
+    pub public_key: String,
+    pub signature: Vec<u8>,
+    pub message: Vec<u8>,
+    pub network_id: i32,
+    pub id: Option<i32>,
+}
+
+impl WalletAuthId {
+    pub fn authenticate(&self) -> Result<bool, AuthIdError> {
+        nacl::sign::verify(
+            &self.signature.as_slice(),
+            &self.message.as_slice(),
+            self.public_key.as_ref(),
+        )
+        .map_err(|z| AuthIdError::GeneralError(format!("invalid wallet")))
+    }
+}
