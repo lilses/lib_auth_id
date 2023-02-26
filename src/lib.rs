1e1c71aab45b5b5626c522a11f2e6194266c0184
evergreen-trading-systems <4870868+evergreen-trading-systems@users.noreply.github.com>
2023-02-26 17:52:41 +0000

    feat: upgrade authentication

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ make_error2!(AuthIdError);
     utoipa::ToSchema, Debug, PartialEq, serde::Deserialize, serde::Serialize, Clone, Default,
 )]
 pub struct WalletAuthId {
-    pub public_key: String,
+    pub public_key: Vec<u8>,
     pub signature: Vec<u8>,
     pub message: Vec<u8>,
     pub network_id: i32,
@@ -19,9 +19,7 @@ impl WalletAuthId {
         nacl::sign::verify(
             &self.signature.as_slice(),
             &self.message.as_slice(),
-            solana_sdk::pubkey::Pubkey::from_str(self.public_key.as_str())
-                .map_err(AuthIdError::from_general)?
-                .as_ref(),
+            solana_sdk::pubkey::Pubkey::new(self.public_key.as_slice()).as_ref(),
         )
         .map_err(|z| AuthIdError::GeneralError(z.message))
     }
